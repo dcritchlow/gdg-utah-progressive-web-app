7142d5fc078d851dfd31a98a95f8fe501e67d1f8
Darin Critchlow <darincritchlow@gmail.com>
2016-08-09 22:20:43 -0600

    Adjust project for Cache Storage
    Moved css and js files from http server so that ServiceWorker can cache them

--- sw.js
+++ sw.js
@@ -1,7 +1,33 @@
 console.log("SW startup");
 
 self.addEventListener('install', function(event) {
-    console.log("SW installed");
+    var urlsToCache = [
+        '/',
+        'css/9-layout.css',
+        'css/bootstrap.min.css',
+        'css/jquery.bxslider.css',
+        'css/jquery.magnificpopup.css',
+        'css/mono-social-icons.css',
+        'css/skin-57830b7bdca26.css',
+        'css/style.css',
+        'js/9-layout.js',
+        'js/bootstrap.min.js',
+        'js/jquery-migrate.min.js',
+        'js/jquery.bxslider.min.js',
+        'js/jquery.easing.1.3.js',
+        'js/jquery.fitvids.js',
+        'js/jquery.js',
+        'js/jquery.magnificpopup.min.js',
+        'js/jquery.mosaicflow.min.js',
+        'js/jquery.throttle.min.js',
+        'js/jquery.wookmark.min.js',
+        'js/theme.js',
+        'js/wp-embed.min.js',
+    ];
+    event.waitUntil(
+        // TODO: open a cache  named 'devfestfam-static-v1'
+        // And cache the urls from urlsToCache
+    );
 });
 
 self.addEventListener('activate', function(event) {
