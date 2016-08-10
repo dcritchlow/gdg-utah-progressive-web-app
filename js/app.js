ce346c5a8f43b421390c00012520f42b6d889938
Darin Critchlow <Darin.Critchlow@selecthealth.org>
2016-08-10 12:01:02 -0600

    Notifying users there is an updated ServiceWorker
    The notification does not update the page yet

--- js/app.js
+++ js/app.js
@@ -17,20 +17,45 @@ toastr.options = {
     "tapToDismiss": false
 }
 
+function trackChanges(worker){
+    worker.addEventListener('statechange', function(){
+        if(worker.state == 'installed'){
+            sendMessage();
+        }
+    })
+}
+
+function sendMessage(){
+    toastr.info("A new version is available<br /><br /><button type='button' class='btn clear'>Update</button>", "Would you like to update?");
+}
+
 navigator.serviceWorker.register('/sw.js').then(function(reg) {
     // TODO: if there's no controller, this page wasn't loaded
     // via a service worker, so they're looking at the latest version.
     // In that case, exit early
+    if(!navigator.serviceWorker.controller) return;
 
-    // TODO: if there's an updated worker already waiting, send a 
+    // TODO: if there's an updated worker already waiting, send a
     // message to the user
+    if (reg.waiting){
+        sendMessage();
+        return;
+    }
 
     // TODO: if there's an updated worker installing, track its
     // progrees. It if becomes "installed", send a message to the user
+    if(reg.installing){
+        // track changes reg.installing == worker
+        trackChanges(reg.installing);
+        return;
+    }
 
     // TODO: otherwise, listen for new installing workers arriving.
     // If one arrives, track its progress.
     // If it becomes "installed", send message to user
+    reg.addEventListener('updatefound', function(){
+        trackChanges(reg.installing);
+    });
 
 }, function(err) {
     console.log('ಠ_ಠ', err);
