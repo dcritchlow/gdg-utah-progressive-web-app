da2bd29c49d9486a69b05da41c0ed5e4f779e12c
Darin Critchlow <Darin.Critchlow@selecthealth.org>
2016-08-10 12:49:20 -0600

    Update button click reloads page and service worker

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ toastr.options = {
     "progressBar": false,
     "positionClass": "toast-bottom-full-width",
     "preventDuplicates": false,
-    "onclick": function(e){ console.log(e); },
+    "onclick": function(){refreshPage(_worker)},
     "showDuration": "300",
     "hideDuration": "1000",
     "timeOut": 0,
@@ -17,6 +17,7 @@ toastr.options = {
     "tapToDismiss": false
 }
 
+var _worker = {};
 
 function trackChanges(worker){
     worker.addEventListener('statechange', function(){
@@ -26,7 +27,8 @@ function trackChanges(worker){
     })
 }
 
-function sendMessage(){
+function sendMessage(worker){
+    _worker = worker;
     toastr.info(
         "A new version is available<br /><br /><button type='button' class='btn clear'>Update</button>",
         "Would you like to update?"
@@ -34,27 +36,42 @@ function sendMessage(){
 }
 
 // TODO: add function that will tell the service worker to skipWaiting when update btn clicked
+function refreshPage(worker){
+    worker.postMessage({action: 'skipWaiting'});
+}
 
 navigator.serviceWorker.register('/sw.js').then(function(reg) {
+    // TODO: if there's no controller, this page wasn't loaded
+    // via a service worker, so they're looking at the latest version.
+    // In that case, exit early
     if(!navigator.serviceWorker.controller) return;
 
+    // TODO: if there's an updated worker already waiting, send a
+    // message to the user
     if (reg.waiting){
-        sendMessage();
+        sendMessage(reg.waiting);
         return;
     }
 
+    // TODO: if there's an updated worker installing, track its
+    // progrees. It if becomes "installed", send a message to the user
     if(reg.installing){
         trackChanges(reg.installing);
         return;
     }
 
+    // TODO: otherwise, listen for new installing workers arriving.
+    // If one arrives, track its progress.
+    // If it becomes "installed", send message to user
     reg.addEventListener('updatefound', function(){
         trackChanges(reg.installing);
     });
-
 }, function(err) {
     console.log('ಠ_ಠ', err);
 });
 
 
 // TODO: listen for the controlling service worker changing and reload the page
+navigator.serviceWorker.addEventListener('controllerchange', function(){
+    window.location.reload();
+});
